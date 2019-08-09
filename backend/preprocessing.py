c3d2986dc9e5f6bb71be49cd8ff5fc2ae2e41581
Adrian Zuber <xadrianzetx@gmail.com>
2019-08-09 18:26:23 +0200

    mv set start and set end into one method

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -10,8 +10,7 @@ class TripTimePreprocessor:
     def __init__(self):
         self._data = np.zeros((7, ))
         self._coords = self._load_lookup()
-        self._start_set = False
-        self._end_set = False
+        self._set = [False, False]
 
     @staticmethod
     def _load_lookup():
@@ -27,10 +26,11 @@ class TripTimePreprocessor:
 
         return coords
 
-    def set_start_point(self, station_id):
+    def set(self, station_id, mode):
         """
 
         :param station_id:
+        :param mode:
         :return:
         """
         valid_ids = [int(i) for i in self._data.keys()]
@@ -44,42 +44,24 @@ class TripTimePreprocessor:
         if int(station_id) not in valid_ids:
             raise KeyError('Invalid station id value passed to preprocessor')
 
-        # load values from lookup
-        alt = self._coords[station_id]['altitude']
-        coords = self._coords[station_id]['data']
-
-        # set start values in correct order
-        self._data[0] = coords[0]
-        self._data[1] = coords[1]
-        self._data[2] = alt
-        self._start_set = True
-
-    def set_end_point(self, station_id):
-        """
-
-        :param station_id:
-        :return:
-        """
-        valid_ids = [int(i) for i in self._data.keys()]
-
-        try:
-            int(station_id)
-
-        except (ValueError, TypeError):
-            raise KeyError('Invalid station id type passed to preprocessor')
-
-        if int(station_id) not in valid_ids:
-            raise KeyError('Invalid station id value passed to preprocessor')
+        if mode not in ['start', 'end']:
+            raise ValueError('')
 
         # load values from lookup
         alt = self._coords[station_id]['altitude']
         coords = self._coords[station_id]['data']
 
-        # set start values in correct order
-        self._data[3] = coords[0]
-        self._data[4] = coords[1]
-        self._data[5] = alt
-        self._end_set = True
+        if mode == 'start':
+            self._data[0] = coords[0]
+            self._data[1] = coords[1]
+            self._data[2] = alt
+            self._set[0] = True
+
+        else:
+            self._data[3] = coords[0]
+            self._data[4] = coords[1]
+            self._data[5] = alt
+            self._set[1] = True
 
     def transform(self):
         pass
