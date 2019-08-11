809f8bb9b253e3646d703b68f224625f9386c28d
Adrian Zuber <xadrianzetx@gmail.com>
2019-08-11 13:05:02 +0200

    add leaflet map to UI

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([37.773135, -122.418920], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: config['mapboxApiKey']
+}).addTo(map);
