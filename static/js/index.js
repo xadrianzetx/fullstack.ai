7a9dc1ff38a6f3cf613c67352cd841d229a87ab3
Adrian Zuber <xadrianzetx@gmail.com>
2019-08-15 13:34:19 +0200

    toggle notes with updated info

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,3 @@
-$('.notice-current').toggle();
-$('.notice-popular-0').toggle();
-$('.notice-popular-1').toggle();
-$('.notice-popular-2').toggle();
-$('.notice-end').toggle();
-$('.notice-predicted').toggle();
-
 const handler = new MapEventHandler();
 
 function onMarkerClick(data) {
@@ -12,9 +5,10 @@ function onMarkerClick(data) {
     if (!handler.isStartPointSet && !handler.isPredicted) {
         // new starting point
         handler.setStartPoint(data);
-        var popularStations = data.target.options.popular
+        var popularStations = data['target']['options']['popular'];
         var keys = Object.keys(popularStations);
         var startCoords = data['latlng'];
+        var startName = data['target']['options']['name'];
 
         for (key in popularStations) {
             // get top 3 most popular endpoints from current station
@@ -28,20 +22,20 @@ function onMarkerClick(data) {
             L.polyline(latlng).addTo(map);
         }
 
+        $('.notice-current').html('<strong>Start</strong> ' + startName);
+        $('.notice-current').toggle();
+
         for (i = 0; i <= 2; i++) {
+            // toggle most popular stations
             var key = keys[i];
             var name = popularStations[key]['name'];
             $('.notice-popular-' + i).toggle();
             $('.notice-popular-' + i).html('<strong>Popular destination</strong> ' + name);
         }
 
-        $('.notice-current').toggle();
-
     } else if (handler.isStartPointSet && !handler.isPredicted) {
-        // when starting point has been set
         // set end point and await prediction
         handler.setEndPoint(data).then(function (datum) {
-            // clear most popular routes
             clearAllPolylines();
 
             var startCoords = handler.startStationCoords;
@@ -51,16 +45,24 @@ function onMarkerClick(data) {
                 [currentCoords['lat'], currentCoords['lng']]
             ];
 
-            // add polyline for selected one
             L.polyline(latlng).addTo(map);
 
-            $('.notice-popular-0').toggle();
-            $('.notice-popular-1').toggle();
-            $('.notice-popular-2').toggle();
+            for (i = 0; i <= 2; i++) {
+                // turn off most popular routes
+                $('.notice-popular-' + i).toggle();
+            }
+            
+            // parse time
+            var min = parseInt(datum['predicted']);
+            var sec = Math.round((datum['predicted'] - min) * 60);
+            var total = min + ' minutes ' + sec + ' seconds'
+            var endName = data['target']['options']['name'];
+            
+            // toggle notes with updated text
+            $('.notice-predicted').html('<strong>Predicted trip time</strong> ' + total);
+            $('.notice-end').html('<strong>End</strong> ' + endName);
             $('.notice-end').toggle();
             $('.notice-predicted').toggle();
-
-            console.log(datum['predicted']);
         });
 
     }
@@ -81,9 +83,10 @@ function onMapClick(data) {
         handler.startPointSet = false;
         clearAllPolylines();
         $('.notice-current').toggle();
-        $('.notice-popular-0').toggle();
-        $('.notice-popular-1').toggle();
-        $('.notice-popular-2').toggle();
+        
+        for (i = 0; i <= 2; i++) {
+            $('.notice-popular-' + i).toggle();
+        }
 
     }
 }
@@ -120,11 +123,11 @@ $.ajax({
     contentType: 'application/json',
     success: function(data) {
         for (var key in data) {
-            // load all stations to map with id and metadata
+            // load all stations with id and metadata
             var station = data[key]
             var marker = L.marker(
                 [station['latitude'], station['longitude']],
-                {id: key, popular: data[key]['pop_dest']}
+                {id: key, name: station['name'], popular: data[key]['pop_dest']}
             );
 
             marker.addTo(map);
